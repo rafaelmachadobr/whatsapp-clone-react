0b03da1b6bf741e26ffc800763d67d0457b19af0
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-03-26 14:25:16 -0300

    Adicionando Firebase no projeto

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyAmzeJh9Ek3zHRUgoo-26qg4yWUua7Ih84",
+  authDomain: "whatsappclone-ccd04.firebaseapp.com",
+  projectId: "whatsappclone-ccd04",
+  storageBucket: "whatsappclone-ccd04.appspot.com",
+  messagingSenderId: "377335867017",
+  appId: "1:377335867017:web:93f9d92f4e697e6c758858",
+};
