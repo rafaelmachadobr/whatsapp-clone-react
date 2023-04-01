87626bae92d546a82b5b04e99dd75b78b8c31266
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-04-01 12:06:56 -0300

    Estilizando o login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./styles.css";
 import Api from "../../Api";
 
-export default function Login({onReceive}) {
+import img from "../../imgs/login.svg";
+
+import FacebookIcon from "@mui/icons-material/Facebook";
+import GoogleIcon from "@mui/icons-material/Google";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+export default function Login({ onReceive }) {
   const handleFacebookLogin = async () => {
     let result = await Api.fbPopup();
     if (result) {
@@ -13,16 +19,57 @@ export default function Login({onReceive}) {
   };
 
   return (
-    <div className="login">
-      <h1>WhatsApp Clone</h1>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/598px-WhatsApp.svg.png"
-        alt="WhatsApp"
-      />
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Senha" />
-      <button className="entrar">Entrar</button>
-      <button onClick={handleFacebookLogin}>Entrar com o Facebook</button>
+    <div className="container-login">
+      <div className="img-box">
+        <img src={img} alt="WhatsApp" />
+      </div>
+      <div className="content-box">
+        <div className="form-box">
+          <h2>WhatsApp - React</h2>
+          <form>
+            <div className="input-box">
+              <span>Email</span>
+              <input type="text" placeholder="Email" />
+            </div>
+            <div className="input-box">
+              <span>Senha</span>
+              <input type="password" placeholder="Senha" />
+            </div>
+
+            <div className="remember">
+              <label>
+                <input type="checkbox" /> Lembrar-me
+              </label>
+              <a href="/">Esqueceu a senha?</a>
+            </div>
+
+            <div className="input-box">
+              <input type="submit" value="Entrar" />
+            </div>
+
+            <div className="input-box">
+              <p>
+                Não tem uma conta? <a href="/">Inscrever-ser</a>
+              </p>
+            </div>
+          </form>
+          <h3>Logar com</h3>
+          <ul className="ul">
+            <li>
+              <FacebookIcon
+                onClick={handleFacebookLogin}
+                style={{ color: "#3b5998", fontSize: "40px" }}
+              />
+            </li>
+            <li>
+              <GoogleIcon style={{ color: "#db4437", fontSize: "40px" }} />
+            </li>
+            <li>
+              <TwitterIcon style={{ color: "#1da1f2", fontSize: "40px" }} />
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
